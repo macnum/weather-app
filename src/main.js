8d1bd537cce0969c45c9fe377041ec1c9b3f6656
Macnum <w.damilola@yahoo.com>
2026-02-10 20:22:24 +0100

    feat: add initial codebase

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const li = document.querySelector('.main-container');
+const nav = document.querySelector('.nav');
+
+function weatherApp() {
+	console.log('Go');
+}
+document.addEventListener('DOMContentLoaded', () => {
+	weatherApp();
+});
